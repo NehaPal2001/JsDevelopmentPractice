fee2ccb694c6bcc8f427b3fb9408171b72c377bf
Nehapal7791 <palneha671@gmail.com>
2024-08-16 17:52:50 +0530

    destructuring

---
+++ senerioforDestructuringIssues.js
@@ -0,0 +1,23 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+var someName = "neha";
+// let arrowFunctionSyntax = (name) => {
+//     name="something"
+//     console.log(name);
+// };
+// arrowFunctionSyntax(name);
+// console.log(name);
+
+// let a =(1).toString();
+// Destructuring error code
+
+// var newfunction = (1) =>
+// {
+//     someName = "some"
+//     console.log(someName)
+// };
+
+// solution is to pass value with proper string literal
+newfunction(someName);
+console.log(someName);
