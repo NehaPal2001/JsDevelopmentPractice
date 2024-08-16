e976d7126db8ed56e87d4de7e54780325c855457
Nehapal7791 <palneha671@gmail.com>
2024-08-16 10:00:20 +0530

    day 2 practice code

---
+++ day2.js
@@ -0,0 +1,19 @@
+//Hoisting
+getName(); //it will work but with arrow function it not work because
+console.log(x);
+var x = 1;
+function getName() {
+  console.log("Hello");
+}
+getName2(); //this is arrow function, getName here is behave like variable
+var getName2 = () => {
+  console.log("HEllO");
+};
+//function expression
+const regularFunction = function (param1, param2) {};
+
+// HOF which takes function as arugment and return function as result
+const name = "neha";
+function modify() {
+  name = "some";
+}
