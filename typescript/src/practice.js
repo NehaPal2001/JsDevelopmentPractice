1fe29f99226f3053617cefc64b092f376612267d
Nehapal7791 <palneha671@gmail.com>
2024-09-04 18:03:44 +0530

    typescript practice

---
+++ typescript/src/practice.js
@@ -0,0 +1,4 @@
+console.log("hello world");
+var age = 20;
+age.toFixed();
+console.log(typeof age);
--- typescript/src/practice.ts
+++ typescript/src/practice.ts
@@ -1,2 +1,37 @@
 console.log("hello world");
-let age: number = 20;
+let age = 20;
+age.toFixed();
+console.log(typeof age);
+
+interface User {
+  id: string | number;
+  name: string;
+  age?: number; // optional property
+}
+
+interface Employee extends User {
+  department: string;
+}
+
+class Manager implements Employee {
+  id: number;
+  name: string;
+  age?: number;
+  department: string;
+
+  constructor(id: number, name: string, department: string) {
+    this.id = id;
+    this.name = name;
+    this.department = department;
+  }
+}
+
+type ID = string | number;
+
+type User2 = {
+  id: ID;
+  name: string;
+  age?: number; // optional property
+};
+
+type Log = (message: string) => void;
